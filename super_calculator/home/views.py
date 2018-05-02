7ab4042d18fac74c2f415e3b8cabdcd2741d7111
codyb29 <codybarrett29@gmail.com>
2018-05-02 16:18:10 -0400

    Creating a development branch, and setting up things to prepare for findpi feature.

--- super_calculator/home/views.py
+++ super_calculator/home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home/homepage.html') 
+
+def pi_tool(request):
+    return render(request, 'home/findnth/pi-tool.html') 
